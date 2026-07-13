32d4df7754f5a3138c30b6a3fb71d84f25fce8c2
veradana1993-gif <veradana1993@gmail.com>
2026-07-13 18:28:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,30 @@
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
 
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    CallbackQueryHandler,
+    ContextTypes,
+)
+
+from github import Github
+import json
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+REPO_NAME = "veradana1993-gif/-Los-Faroles-Menu"
+FILE_PATH = "menu.json"
+
+menu = {
+    "primerPlato": "",
+    "segundoPlato": "",
+    "postre": "",
+    "bebida": "",
+    "precio": ""
+}
